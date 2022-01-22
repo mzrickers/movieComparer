086cd557c5477f170a8e877f2000e1d2283e8653
Zach Rickers <mzrickers@gmail.com>
2022-01-22 14:36:41 +0100

    connected to omdbAPI

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com', {
+    params: {
+      apikey: 'b30415c7',
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
