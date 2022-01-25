f4f1bab8bd06958fdb35962649441fc71e8f3aab
Zach Rickers <mzrickers@gmail.com>
2022-01-25 20:53:55 +0100

    add click event

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async event => {  
   const movies = await fetchData(event.target.value);
 
+  if (!movies.length) {
+    dropdown.classList.remove('is-active');
+    return;
+  }
+
   resultsWrapper.innerHTML = '';
   dropdown.classList.add('is-active');
   for (let movie of movies) {
@@ -42,6 +47,11 @@ const onInput = async event => {
     <img src="${imgSrc}" />
     ${movie.Title}
     `;
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+    });
+
 
     resultsWrapper.appendChild(option);
   }
